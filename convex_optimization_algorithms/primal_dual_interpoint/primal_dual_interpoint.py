253625b52d89436ec2692508fbd6535774117318
Takamasa Horibe <horibe.takamasa@gmail.com>
2020-04-29 01:49:25 +0900

    update
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- convex_optimization_algorithms/primal_dual_interpoint/primal_dual_interpoint.py
+++ convex_optimization_algorithms/primal_dual_interpoint/primal_dual_interpoint.py
@@ -6,11 +6,33 @@ import math
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.linalg as linalg
+
 
 class PrimalDualInterpoint():
 
+    class Result():
+        def __init__(self, i=-1, mu=0, alpha=0, x=np.matrix([]), y=np.matrix([]), \
+                     z=np.matrix([]), dx=np.matrix([]), dy=np.matrix([]), dz=np.matrix([])):
+            self.i = i
+            self.mu = mu
+            self.alpha = alpha
+            self.x = x
+            self.y = y
+            self.z = z
+            self.dx = dx
+            self.dy = dy
+            self.dz = dz
+
     def __init__(self):
         self.initialized = False
+        self.mu0 = 100.0
+        self.sigma = 0.01  # about 0.1 ~ 0.01
+        self.tau = 0.95    # about 0.95 ~ 0.99
+        self.max_loop_num = 100
+        self.tol_mu = 0.001
+        self.results = []
+        
 
     def setProblem(self, Q, c, A, b):
         self.Q = Q
@@ -20,21 +42,98 @@ class PrimalDualInterpoint():
         self.N = len(c)
         self.M = len(b)
 
+        self.initialized = True
+
+
+    def getInitialGuess(self):
+        x = np.matrix(np.ones((self.N, 1))) * self.mu0
+        y = np.matrix(np.zeros((self.M, 1)))
+        z = np.matrix(np.ones((self.N, 1))) * self.mu0
+        return (x, y, z)
 
     def solve(self):
         if not self.initialized:
             return
 
-    def setInitialGuess(self, x, y, z):
-        x = np.matrix()
-        self.initialized = True
-        return
+        (x, y, z) = self.getInitialGuess()
+        mu = self.calcDualGap(x, z)
+        self.results.append(PrimalDualInterpoint.Result(i=0, mu=mu, x=x, y=y, z=z))
 
-    
+        for iter_num in range(1, self.max_loop_num):
+            (A_pd, B_pd) = self.calcPrimalDualMatrix(x, y, z, mu)
+            deltas = solveLinEq(A_pd, B_pd)
+            dx = deltas[0:self.N, 0]
+            dy = deltas[self.N:self.N + self.M, 0]
+            dz = deltas[self.N + self.M:, 0]
+            alpha = self.calcStepRatio(x, z, dx, dz)
+
+            x = x + alpha * dx
+            y = y + alpha * dy
+            z = z + alpha * dz
+
+            mu = self.calcDualGap(x, z)
+            self.results.append(PrimalDualInterpoint.Result(i=iter_num, mu=mu, alpha=alpha, x=x, y=y, \
+                                                            z=z, dx=dx, dy=dy, dz=dz))
+
+            print("i = ", iter_num)
+            print("alpha = ", alpha)
+            print("x = ", x.transpose())
+            print("y = ", y.transpose())
+            print("z = ", z.transpose())
+            print("dx = ", dx.transpose())
+            print("dy = ", dy.transpose())
+            print("dz = ", dz.transpose())
+            print("mu = ", mu)
+
+            if abs(mu) < self.tol_mu:
+                print("optimization succeeded")
+                break
+
+    print("over max iteration num.")
+
+    def calcStepRatio(self, x, z, dx, dz):
+        some_larger_num_than_1 = 100.0
+        ax = np.ones(self.N) * some_larger_num_than_1
+        az = np.ones(self.N) * some_larger_num_than_1
+        for i in range(self.N):
+            if dx[i] < 0.0:
+                ax[i] = -x[i] / dx[i]
+            if dz[i] < 0.0:
+                az[i] = -z[i] / dz[i]
+        a = min(self.tau * np.amin(ax), self.tau * np.amin(az), 1.0)
+        if a < -1.0E-5:
+            print("!!!!!!!! ERROR !!!!!!!!!!!!!!!, a = ", a)
+        # print("np.array(x) = ", np.array(x))
+        # print("np.array(dx) = ", np.array(dx))
+        # print("ax = ", ax)
+        # print("ax.min = ", np.amin(ax))
+        return a
+
+
+    def calcPrimalDualMatrix(self, x, y, z, mu):
+        N = self.N
+        M = self.M
+        Z = np.diag(np.array(z).reshape(N,))
+        X = np.diag(np.array(x).reshape(N,))
+        Aex = np.block([[self.A, np.zeros((M, M)), np.zeros((M, N))],
+                        [-self.Q, self.A.transpose(), np.eye(N)],
+                        [Z, np.zeros((N, N)), X]])
+        Bex = np.block([[self.b - self.A * x],
+                        [self.c - self.Q * x - self.A.transpose() * y - z],
+                        [self.sigma * mu * np.ones((N, 1)) - X * z]])
+        # print("Aex = ", Aex)
+        # print("Bex = ", Bex)
+        return (Aex, Bex)
+
     def calcDualGap(self, x, z):
-        n = len(x)
-        mu = (x * z) / n
-        return mu
+        mu = (x.transpose() * z) / self.N
+        return mu.item()
+
+def solveLinEq(A, b):
+    LU = linalg.lu_factor(A)  # LU : much faster than pinv
+    delta = linalg.lu_solve(LU, b)
+    # print("delta = ", delta)
+    return np.matrix(delta)
 
 def plotQP(Q, c, A, b):
 
@@ -70,11 +169,18 @@ def main():
     b = np.matrix([[-1.0],
                    [ 5.0]])
 
-    plotQP(Q, c, A, b)
+    # plotQP(Q, c, A, b)
 
     obj = PrimalDualInterpoint()
     obj.setProblem(Q, c, A, b)
     obj.solve()
+
+    print("|  i   |       mu       | alpha  |               x                |")
+    print("-------------------------------------------------------------------")
+    for r in obj.results:
+        print('{0: 2d}'.format(r.i), '{0: 13.5f}'.format(r.mu), '{0: 6.3f}'.format(r.alpha), r.x.transpose())
+
+
 
 
 if __name__ == "__main__":
